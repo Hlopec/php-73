4cb5dd34764f1144106ff361f7e38f91defe284c
Hlopec <vasylkruten@gmail.com>
2021-01-28 01:50:18 +0200

    array tests

--- tests/ArraysTest.php
+++ tests/ArraysTest.php
@@ -62,17 +62,17 @@ class ArraysTest extends TestCase
         // Union
         $arr1 = ['a', 'b'];
         $arr2 = ['c', 'd', 'e', 'key' => 'value'];
-//        $this->assertEquals(?, $arr1 + $arr2);
+        $this->assertEquals([ '0' => 'a', '1' =>'b', '2' =>'e', 'key' =>'value'], $arr1 + $arr2);
 
         // array_merge — Merge one or more arrays
         $arr1 = ['a', 'b', 'key1' => 'value1', 'key2' => 'value2'];
         $arr2 = [1, 'key1' => 'value3', 'key3' => 'value4'];
-        //$this->assertEquals(?, array_merge($arr1, $arr2));
+        $this->assertEquals(['0' => 'a', '1' => 'b', 'key1' => 'value3', 'key2' => 'value2', '2' => 1, 'key3' => 'value4'], array_merge($arr1, $arr2));
 
         // array_replace — Replaces elements from passed arrays into the first array
         $arr1 = ['a', 'b', 'key1' => 'value1', 'key2' => 'value2'];
         $arr2 = [1, 'key1' => 'value3', 'key3' => 'value4'];
-        //$this->assertEquals(?, array_replace($arr1, $arr2));
+        $this->assertEquals(['0' => 1, '1' => 'b', 'key1' => 'value3', 'key2' => 'value2', 'key3' => 'value4'], array_replace($arr1, $arr2));
     }
 
     /**
@@ -83,11 +83,11 @@ class ArraysTest extends TestCase
     {
         // list - Assign variables as if they were an array
         list(,, list($var)) = ['a', 'b', ['c', 'd']];
-        //$this->assertEquals('c', $var);
+        $this->assertEquals('c', $var);
 
         // implode — Join array elements with a string
         $var = implode([1, 2, 3, 4]);
-        //$this->assertEquals(?, $var);
+        $this->assertEquals('1234', $var);
 
         // sizeof — Alias of count
         // TODO to be implemented
@@ -95,65 +95,81 @@ class ArraysTest extends TestCase
         // unset — Unset a given variable
         $arr = [1, 2, 3, 4];
         unset($arr[0]);
-        //$this->assertEquals(?, array_keys($arr));
+        $this->assertEquals(['0' => '1', '1' => '2', '2' => '3'], array_keys($arr));
 
         // isset — Determine if a variable is declared and is different than NULL
         $arr = [1, 2, 'key' => 'value', null];
-        //$this->assertEquals(?, isset($arr[0]));
-        //$this->assertEquals(?, isset($arr['key']));
-        //$this->assertEquals(?, isset($arr[2]));
+        $this->assertEquals(true, isset($arr[0]));
+        $this->assertEquals(true, isset($arr['key']));
+        $this->assertEquals(false, isset($arr[2]));
 
         // array_key_exists — Checks if the given key or index exists in the array
         $arr = [1, 2, 'key' => 'value', null];
-        //$this->assertEquals(?, array_key_exists(0, $arr));
-        //$this->assertEquals(?, array_key_exists('key', $arr));
-        //$this->assertEquals(?, array_key_exists(2, $arr));
+        $this->assertEquals(true, array_key_exists(0, $arr));
+        $this->assertEquals(true, array_key_exists('key', $arr));
+        $this->assertEquals(true, array_key_exists(2, $arr));
 
         // in_array — Checks if a value exists in an array
         // TODO to be implemented
+        $this->assertEquals(true, in_array(2, $arr));
 
         // array_flip — Exchanges all keys with their associated values in an array
         // TODO to be implemented
+        $arr = [1, 2, 'key' => 'value'];
+        $this->assertEquals(['1' => '0', '2' => '1', 'value' => 'key'], array_flip($arr));
 
         // array_reverse — Return an array with elements in reverse order
         // TODO to be implemented
+        $this->assertEquals([ 'key' => 'value', 2, 1], array_reverse($arr));
 
         // array_keys — Return all the keys or a subset of the keys of an array
         // TODO to be implemented
+        $this->assertEquals([0, 1, 'key'], array_keys($arr));
 
         // array_values — Return all the values of an array
         // TODO to be implemented
+        $this->assertEquals([1, 2, 'value'], array_values($arr));
 
         // array_filter — Filters elements of an array using a callback function
         $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        //$this->assertEquals(?, array_filter($arr, function ($value) {
-        //    return $value % 2 > 0;
-        //}));
+        $this->assertEquals(['0' => '1', '2' => '3', '4' => '5', '6' => '7', '8' => '9'], array_filter($arr, function ($value) {
+            return $value % 2 > 0;
+        }));
 
         // array_map — Applies the callback to the elements of the given arrays
         $arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        //$this->assertEquals(?, array_map(function ($value) {
-        //    return $value * 2;
-        //}, $arr));
+        $this->assertEquals([2, 4, 6, 8, 10, 12, 14, 16, 18, 20], array_map(function ($value) {
+            return $value * 2;
+        }, $arr));
 
         // sort — Sort an array
         // TODO to be implemented
+        $arr = [1, 5, 3, 2, 4];
+//        $this->assertEquals([1, 2, 3, 4, 5], sort($arr));
 
         // rsort — Sort an array in reverse order
         // TODO to be implemented
+//        $this->assertEquals([5, 4, 3, 2, 1], rsort($arr));
 
         // ksort — Sort an array by key
         // TODO to be implemented
+//        $this->assertEquals([0, 1, 2, 3, 4], ksort($arr));
 
         // usort — Sort an array by values using a user-defined comparison function
         // TODO to be implemented
+//        $this->assertEquals(?, usort($arr));
 
+        $arr = [1, 2];
         // array_push — Push one or more elements onto the end of array
         // array_pop — Pop the element off the end of array
         // TODO to be implemented
+//        $this->assertEquals([1, 2, 3], array_push($arr, "3"));
+//        $this->assertEquals([1], array_pop($arr));
 
         // array_shift — Shift an element off the beginning of array
         // array_unshift — Prepend one or more elements to the beginning of an array
         // TODO to be implemented
+//        $this->assertEquals([2], array_shift($arr));
+//        $this->assertEquals([1, 2], array_unshift($arr, 1));
     }
 }
